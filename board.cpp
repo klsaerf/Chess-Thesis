9499506a297d3ce4047f103aca0bf0e726c5eea3
Kaan Pide <kaanpd@gmail.com>
2026-02-01 23:00:50 +0200

    Implemented getAvailableMoves and improved makeMove

--- board.cpp
+++ board.cpp
@@ -6,6 +6,16 @@
 #include "board.h"
 #include <iostream>
 
+// Returns the color of the piece
+Color getColor(const char piece) {
+    if (piece == '0') return EMPTY;
+    return (isupper(piece)) ? WHITE : BLACK;
+}
+
+bool isInBounds(const int x, const int y) {
+    return x >= 0 && x < BOARD_LENGTH && y >= 0 && y < BOARD_LENGTH;
+}
+
 Board::Board(): whiteCanCastleShort_(true), whiteCanCastleLong_(true),
                 blackCanCastleShort_(true), blackCanCastleLong_(true) {
     board_.reserve(BOARD_SIZE);
@@ -87,12 +97,208 @@ bool Board::makeMove(const int begin, const int end) {
         return false;
     }
 
+    // Get the possible set of moves from the beginning square
+    std::vector<Move> moves;
+    switch (toupper(board_.at(begin))) {
+        case 'P':
+            getPawnMoves(moves, begin);
+            break;
+        case 'N':
+            getKnightMoves(moves, begin);
+            break;
+        case 'B':
+            getBishopMoves(moves, begin);
+            break;
+        case 'R':
+            getRookMoves(moves, begin);
+            break;
+        case 'Q':
+            getQueenMoves(moves, begin);
+            break;
+        case 'K':
+            getKingMoves(moves, begin);
+            break;
+        default:
+            break;
+    }
+
+    // If the piece cannot move there, return false
+    if (std::ranges::find(moves, std::pair(begin, end)) == moves.end()) {
+        return false;
+    }
+
     // Swap the end piece for the beginning piece, and remove the beginning piece
     board_.at(end) = board_.at(begin);
     board_.at(begin) = '0';
+
+    // Promotion to queen
+    if (end / BOARD_LENGTH == BOARD_LENGTH - 1 && board_.at(end) == 'P') {
+        board_.at(end) = 'Q';
+    }
+    if (end / BOARD_LENGTH == 0 && board_.at(end) == 'p') {
+        board_.at(end) = 'q';
+    }
 
     return true;
 }
 
 void Board::getAvailableMoves() {
+    std::vector<Move> moves;
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        switch (const char piece = board_.at(i); toupper(piece)) {
+            case 'P':
+                getPawnMoves(moves, i);
+                break;
+            case 'N':
+                getKnightMoves(moves, i);
+                break;
+            case 'B':
+                getBishopMoves(moves, i);
+                break;
+            case 'R':
+                getRookMoves(moves, i);
+                break;
+            case 'Q':
+                getQueenMoves(moves, i);
+                break;
+            case 'K':
+                getKingMoves(moves, i);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void Board::getPawnMoves(std::vector<Move>& moves, const int index) const {
+    const Color color = getColor(board_.at(index));
+    const Color oppositeColor = color == WHITE ? BLACK : WHITE;
+    // The direction of the pawn
+    const int direction = color == WHITE ? 1 : -1;
+
+    const int x = index % BOARD_LENGTH;
+    const int y = index / BOARD_LENGTH;
+
+    // Check if front square is empty
+    if (board_.at(index + direction * BOARD_LENGTH) == '0') {
+        moves.emplace_back(index, index + direction * BOARD_LENGTH);
+
+        // Check if the pawn is on starting position and both front squares are empty
+        if ((y == 1 && color == WHITE) || (y == BOARD_LENGTH - 2 && color == BLACK)) {
+            if (board_.at(index + direction * BOARD_LENGTH * 2) == '0') {
+                moves.emplace_back(index, index + direction * BOARD_LENGTH * 2);
+            }
+        }
+    }
+
+    // Check to the upper left square of pawn for capture
+    if (x > 0) {
+        if (getColor(board_.at(index + direction * BOARD_LENGTH - 1)) == oppositeColor) {
+            moves.emplace_back(index, index + direction * BOARD_LENGTH - 1);
+        }
+    }
+
+    // Check to the upper right square of pawn for capture
+    if (x < BOARD_LENGTH - 1) {
+        if (getColor(board_.at(index + direction * BOARD_LENGTH + 1)) == oppositeColor) {
+            moves.emplace_back(index, index + direction * BOARD_LENGTH + 1);
+        }
+    }
+}
+
+void Board::getKnightMoves(std::vector<Move>& moves, const int index) const {
+    const Color color = getColor(board_.at(index));
+
+    const int x = index % BOARD_LENGTH;
+    const int y = index / BOARD_LENGTH;
+
+    // Lambda function for knight move
+    auto knightMove = [this](const int index, const int x, const int y, std::vector<Move>& moves, const Color color) {
+        if (isInBounds(x, y) && getColor(board_.at(x + y * BOARD_LENGTH)) != color) {
+            moves.emplace_back(index, x + y * BOARD_LENGTH);
+        }
+    };
+
+    // Check for all 8 possible squares a knight can move
+    knightMove(index, x + 1, y + 2, moves, color);
+    knightMove(index, x - 1, y + 2, moves, color);
+    knightMove(index, x + 2, y + 1, moves, color);
+    knightMove(index, x - 2, y + 1, moves, color);
+    knightMove(index, x + 2, y - 1, moves, color);
+    knightMove(index, x - 2, y - 1, moves, color);
+    knightMove(index, x + 1, y - 2, moves, color);
+    knightMove(index, x - 1, y - 2, moves, color);
+}
+
+void Board::getBishopMoves(std::vector<Move>& moves, const int index) const {
+    const Color color = getColor(board_.at(index));
+
+    const int x = index % BOARD_LENGTH;
+    const int y = index / BOARD_LENGTH;
+
+    bool ppFlag = true;
+    bool pnFlag = true;
+    bool npFlag = true;
+    bool nnFlag = true;
+
+    for (int i = 1; i < BOARD_LENGTH; i++) {
+        if (ppFlag) canMove(index, x + i, y + i, color, moves, ppFlag);
+        if (pnFlag) canMove(index, x + i, y - i, color, moves, pnFlag);
+        if (npFlag) canMove(index, x - i, y + i, color, moves, npFlag);
+        if (nnFlag) canMove(index, x - i, y - i, color, moves, nnFlag);
+    }
+}
+
+void Board::getRookMoves(std::vector<Move>& moves, const int index) const {
+    const Color color = getColor(board_.at(index));
+
+    const int x = index % BOARD_LENGTH;
+    const int y = index / BOARD_LENGTH;
+
+    bool pvFlag = true;
+    bool nvFlag = true;
+    bool phFlag = true;
+    bool nhFlag = true;
+
+    for (int i = 1; i < BOARD_LENGTH; i++) {
+        if (pvFlag) canMove(index, x + i, y, color, moves, pvFlag);
+        if (nvFlag) canMove(index, x + i, y, color, moves, nvFlag);
+        if (phFlag) canMove(index, x, y + i, color, moves, phFlag);
+        if (nhFlag) canMove(index, x, y - i, color, moves, nhFlag);
+    }
+}
+
+void Board::getQueenMoves(std::vector<Move>& moves, const int index) const {
+    getBishopMoves(moves, index);
+    getRookMoves(moves, index);
+}
+
+void Board::getKingMoves(std::vector<Move>& moves, const int index) const {
+    const Color color = getColor(board_.at(index));
+
+    const int x = index % BOARD_LENGTH;
+    const int y = index / BOARD_LENGTH;
+
+    for (int i =-1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (isInBounds(x + i, y + j) && getColor(board_.at(x + i + (y + j) * BOARD_LENGTH)) != color) {
+                moves.emplace_back(index, x + i + (y + j) * BOARD_LENGTH);
+            }
+        }
+    }
+}
+
+void Board::canMove(const int index, const int x, const int y, const Color color, std::vector<Move>& moves, bool& flag) const {
+    const bool inBounds = isInBounds(x, y);
+    if (!inBounds) {
+        flag = false;
+        return;
+    }
+
+    const Color targetColor = getColor(board_.at(x + y * BOARD_LENGTH));
+
+    if (targetColor != color) moves.emplace_back(index, x + y * BOARD_LENGTH);
+
+    flag = targetColor == EMPTY;
 }
--- board.h
+++ board.h
@@ -7,6 +7,8 @@
 
 #include <vector>
 
+using Move = std::pair<int, int>;
+
 // Board constants
 const int BOARD_LENGTH = 8;
 const int BOARD_SIZE = BOARD_LENGTH * BOARD_LENGTH;
@@ -38,12 +40,39 @@ private:
     // Contains the pieces as chars along one dimension for easier management.
     std::vector<char> board_;
 
-    // Flags for castling for both sides and colors
+    // -- Flags for castling for both sides and colors. --
+
     bool whiteCanCastleShort_;
     bool whiteCanCastleLong_;
 
     bool blackCanCastleShort_;
     bool blackCanCastleLong_;
+
+    // -- Private functions for retrieving the possible moves of the given piece. --
+
+    // Extend the move vector with the moves a pawn can make on square index
+    void getPawnMoves(std::vector<Move>& moves, int index) const;
+
+    // Extend the move vector with the moves a knight can make on square index
+    void getKnightMoves(std::vector<Move>& moves, int index) const;
+
+    // Extend the move vector with the moves a bishop can make on square index
+    void getBishopMoves(std::vector<Move>& moves, int index) const;
+
+    // Extend the move vector with the moves a rook can make on square index
+    void getRookMoves(std::vector<Move>& moves, int index) const;
+
+    // Extend the move vector with the moves a queen can make on square index
+    void getQueenMoves(std::vector<Move>& moves, int index) const;
+
+    // Extend the move vector with the moves a king can make on square index
+    void getKingMoves(std::vector<Move>& moves, int index) const;
+
+    // Helper function that updates the move vector for the given target coordinates.
+    // If target is out of bounds or the target square isn't empty, the flag is set to false.
+    // The move is only added if the target square is in bounds and the target square has a different
+    // color than the index color.
+    void canMove(int index, int x, int y, Color color, std::vector<Move>& moves, bool& flag) const;
 };
 
 
